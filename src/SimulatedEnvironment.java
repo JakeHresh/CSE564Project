75dcafbe597af061c21e7b90931b3fe325a1eadf
Jacob Hreshchyshyn <jakeyhresh123@gmail.com>
2023-04-17 10:45:17 -0700

    Added scaffolding to main simulator

--- src/SimulatedEnvironment.java
+++ src/SimulatedEnvironment.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class SimulatedEnvironment {
+    static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Welcome to Team 7's Triage Classification System!");
+        System.out.println("When prompted, you may select a one of the experiments against which you can test this system.");
+        System.out.println("When you wish to exit, enter any character.");
+        String input = "";
+        while(true) {
+            System.out.println("Please enter a number corresponding with the experiment to run.");
+            System.out.println("1. Experiment 1");
+            System.out.println("2. Experiment 2");
+            System.out.println("3. Experiment 3");
+            System.out.println("4. Experiment 4");
+
+            input = scan.nextLine();
+
+            if(input.equals("1")) {
+                System.out.println("Running Experiment 1");
+            }
+            else if(input.equals("2")) {
+                System.out.println("Running Experiment 2");
+            }
+            else if(input.equals("3")) {
+                System.out.println("Running Experiment 3");
+            }
+            else if(input.equals("4")) {
+                System.out.println("Running Experiment 4");
+            }
+            else {
+                break;
+            }
+        }
+        System.out.println("Goodbye!");
     }
 }
